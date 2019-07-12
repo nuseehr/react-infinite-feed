e1d0ee1c646186e3ef8618913c42a4fac8f6f804
Lee Sun Hong <nuseehr@naver.com>
2019-07-13 04:18:09 +0900

    Update package.json

---
+++ build/precache-manifest.bb353cd9ff6271d886a6bdb17c7118f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51995658eb2aa14af5275972fb0f37c2",
+    "url": "/react-infinite-feed/index.html"
+  },
+  {
+    "revision": "c22f857b2165493c42e9",
+    "url": "/react-infinite-feed/static/css/2.b21477da.chunk.css"
+  },
+  {
+    "revision": "35973a104044dacd0559",
+    "url": "/react-infinite-feed/static/css/main.746c7c90.chunk.css"
+  },
+  {
+    "revision": "c22f857b2165493c42e9",
+    "url": "/react-infinite-feed/static/js/2.d6775bcd.chunk.js"
+  },
+  {
+    "revision": "35973a104044dacd0559",
+    "url": "/react-infinite-feed/static/js/main.0f462f0c.chunk.js"
+  },
+  {
+    "revision": "33d9a113a8881935e8e7",
+    "url": "/react-infinite-feed/static/js/runtime~main.dc310a86.js"
+  }
+]);
